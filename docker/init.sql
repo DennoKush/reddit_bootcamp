9b9ad15fb2e8123eb7c0773efcfe9d8c1869710d
Dennis Wanjiku <dwanjiku@mfs.co.ke>
2024-09-07 13:41:20 +0300

    add files

---
+++ docker/init.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE IF NOT EXISTS dim_author (
+    author_id VARCHAR(255) PRIMARY KEY,
+    author_name TEXT
+    );
+
+CREATE TABLE dim_subreddit (
+    subreddit_id VARCHAR(255) PRIMARY KEY,
+    subreddit_name TEXT
+    );
+
+CREATE TABLE IF NOT EXISTS fact_post (
+    post_id VARCHAR(255) PRIMARY KEY,
+    title TEXT,
+    author_id VARCHAR(255),
+    subreddit_id VARCHAR(255),
+    score INT,
+    num_comments INT,
+    created_at TIMESTAMP,
+    url TEXT,
+    selftext TEXT,
+    FOREIGN KEY (author_id) REFERENCES dim_author(author_id),
+    FOREIGN KEY (subreddit_id) REFERENCES dim_subreddit(subreddit_id)
+    );
+
+CREATE TABLE dim_date (
+    date_id DATE PRIMARY KEY,
+    day INT,
+    month INT,
+    year INT,
+    weekday INT
+    );
+
+
+
+
+
+
